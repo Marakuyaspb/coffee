17a3f2c3185de403dbeb3f93897c619bcce4834b
aa <marakuyaspb@gmail.com>
2024-10-15 14:38:25 +0300

    show call requests in admin panel

--- co/main/admin.py
+++ co/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(CallMe)
+class CallMeAdmin(admin.ModelAdmin):
+	list_display = ['first_name', 'phone', 'message', 'created']
